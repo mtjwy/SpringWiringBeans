986203a223b21de0ab3a97cb7f75a0a6124259a6
Yu Wu <wuyucsu@gmail.com>
2015-10-09 12:25:15 -0500

    define interface, add one implementation

---
+++ src/main/java/com/lamaryw/spring/soundsystem/CompactDisc.java
@@ -0,0 +1,12 @@
+package com.lamaryw.spring.soundsystem;
+
+/*
+ * We defined a interface CompactDisc. As an interface, it defines the
+ * contract through which a CDplayer can operate on the CD. And it keeps the
+ * coupling between any CD player implementation and the CD itself to a
+ * minimum.
+ */
+public interface CompactDisc {
+	void play();
+	
+}
---
+++ src/main/java/com/lamaryw/spring/soundsystem/SgtPeppers.java
@@ -0,0 +1,35 @@
+package com.lamaryw.spring.soundsystem;
+
+import org.springframework.stereotype.Component;
+
+/*
+ * @Component annotation identifies this class as a component class
+ * and serves as a clue to Spring that a bean should be created for the class.
+ * 
+ */
+@Component
+public class SgtPeppers implements CompactDisc{
+	
+	private String title;
+	private String artist;
+	
+	public void play() {
+		System.out.println("Playing " + title + "by" + artist);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+}
