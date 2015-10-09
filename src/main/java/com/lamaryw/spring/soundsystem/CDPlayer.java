f0379a6b3639a5bb98fbd665f33c7943b098d34c
Yu Wu <wuyucsu@gmail.com>
2015-10-09 14:10:39 -0500

    create MediaPlayer interface and a CDPlayer class

---
+++ src/main/java/com/lamaryw/spring/soundsystem/CDPlayer.java
@@ -0,0 +1,22 @@
+package com.lamaryw.spring.soundsystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CDPlayer implements MediaPlayer {
+	private CompactDisc cd;
+	
+	/*
+	 * constructor is annotated with @Autowired, indicating that when Spring
+	 * creates the CDPlayer bean it should instantiate it via that constructor
+	 * and pass in a bean that is assignable to CompactDisc.
+	 */
+	@Autowired
+	public CDPlayer(CompactDisc cd) {
+		this.cd = cd;
+	}
+
+	public void play() {
+		cd.play();
+	}
+
+}
---
+++ src/main/java/com/lamaryw/spring/soundsystem/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.lamaryw.spring.soundsystem;
+
+public interface MediaPlayer {
+	void play();
+}
